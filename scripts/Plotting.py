027f74291681c025865b6b4c8e045cce6d565803
Yosef-ft <yosefetene02@gmail.com>
2024-09-19 15:13:19 +0300

    plots for seasonality, correlation and effect of promo

--- scripts/Plotting.py
+++ scripts/Plotting.py
@@ -54,4 +54,71 @@ class Plots:
             plt.show()
 
         except Exception as e:
-            logger.error(f"Error in plotting Outliers: {e}")            
+            logger.error(f"Error in plotting Outliers: {e}")      
+
+
+    def seasonal_purchase_behavior(self, data):
+        '''
+        Creates a bar plot for seasonal purchase behaviour
+        '''
+        try: 
+            logger.debug('Plotting seasonal purchase behaviour of customers...')
+            holiday_sales = data[data['Holiday'] != 'Not Holiday'].groupby(by='Holiday')['Sales'].sum().reset_index().sort_values(by = 'Sales', ascending=False)
+
+            sns.barplot(data=holiday_sales, x='Holiday', y='Sales', hue='Holiday', palette='coolwarm', legend=False)
+            plt.xticks(rotation = 90)
+            plt.title('Sales volume by Holiday')
+            plt.xlabel('Holidays')
+            plt.ylabel('Sales Volume')     
+            plt.show();   
+        except Exception as e:
+             logger.error(f"Error in plotting bar plot for seasonal purchase behaviour: {e}")  
+
+
+    def customer_sales_corr(self, data):
+        '''
+        Scatter plot between sales and customer
+        '''
+        logger.debug('Scatter plot between sales and customer')
+        try: 
+            scatter = plt.scatter(data['Customers'], data['Sales'], c=data.index, cmap='viridis')
+            plt.colorbar(scatter, label='Date')
+            plt.title('Sales vs Customers Over Time')
+            plt.xlabel('Number of Customers')
+            plt.ylabel('Sales')
+            plt.show()  
+        except Exception as e:
+            logger.error(f'Error while plotting scatter plot {e}')      
+
+    def effect_of_promo(self, data, col: str):
+        '''
+        Line plot of the effect of Promo on differnet columns
+
+        Parameters:
+        ----------
+            data(pd.DataFrame)
+            col(str): the column you want to compare with the Promo
+        '''
+        logger.debug(f'line plot between Promo and {col}')
+        try: 
+            sns.lineplot(
+                x=data[data['Promo'] == 1]['Date'].dt.month,
+                y=data[data['Promo'] == 1][col],
+                label='With Promo', 
+                color='darkblue', 
+                linestyle='-', 
+                linewidth=2,
+                marker='o'
+            )
+
+            sns.lineplot(
+                x=data[data['Promo'] == 0]['Date'].dt.month,
+                y=data[data['Promo'] == 0][col],
+                label='Without Promo', 
+                color='salmon', 
+                linestyle='--', 
+                linewidth=2,
+                marker='x'
+            )        
+        except Exception as e:
+            logger.error(f'Error while plotting line plot {e}')    
