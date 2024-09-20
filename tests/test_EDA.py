9c4d614efaf9a9a2a747b98c8f1108ae7a0371cd
Yosef-ft <yosefetene02@gmail.com>
2024-09-20 10:20:46 +0300

    Test for stores_promo function

--- tests/test_EDA.py
+++ tests/test_EDA.py
@@ -22,6 +22,12 @@ class TestEDA(unittest.TestCase):
             'B' : [1, 1, 2, 2, 2, 2]
         })
 
+        self.store_data = pd.DataFrame({
+            'Store': [1, 1, 2, 2, 3, 3, 4, 4, 5, 5],
+            'Promo': [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+            'Sales': [1000, 600, 2000, 1800, 1500, 1100, 500, 500, 3000, 2400]
+        })
+
 
 
     def test_distribution_similar(self):
@@ -45,6 +51,20 @@ class TestEDA(unittest.TestCase):
 
         self.assertAlmostEqual(result, 16.67)
 
+
+    def test_stores_promo(self):
+        '''
+        This function tets the stores promo function by checking the columns
+        '''
+
+        eda = EDA()
+        result = eda.stores_promo(self.store_data)
+        cols = list(result.columns)
+
+        self.assertTrue(isinstance(result.index, pd.Index))
+        
+        expected_columns = ['(No Promo / with Promo)%', 'store no promo', 'store with promo']
+        self.assertListEqual(list(result.columns), expected_columns)
 
 if __name__ == '__main__':
     unittest.main()
