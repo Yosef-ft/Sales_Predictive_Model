b1f4ba65fbd6677687eb0d99ae885392e7b52776
Yosef-ft <yosefetene02@gmail.com>
2024-09-21 13:21:21 +0300

    Custom transformer for changing columns to proper dtypes

--- scripts/Custom_Transformers.py
+++ scripts/Custom_Transformers.py
@@ -9,6 +9,16 @@ from sklearn.impute import SimpleImputer
 class MissingDataHandler(BaseEstimator, TransformerMixin):
     '''
     Custom Trnasformer for handling missing data
+
+    Parameters:
+    -----------
+        strategy(str):
+        fill_value:
+        missing_values: 
+
+    Returns:
+    --------
+
     '''
     def __init__(self, strategy, fill_value = None ,missing_values = np.nan):
         self.missing_values= missing_values
@@ -59,8 +69,10 @@ class OutlierHandler(BaseEstimator, TransformerMixin):
         return self
     
     def transform(self, X, y=None):
+
+        X_copy = X.copy()
         
-        numeric_col = X.select_dtypes(include='float64').columns
+        numeric_col = X_copy.select_dtypes(include='float64').columns
         if self.cols:
             fix_cols = [col for col in numeric_col if col not in self.cols]
         else:
@@ -68,21 +80,54 @@ class OutlierHandler(BaseEstimator, TransformerMixin):
 
         if self.method == 'IQR':
 
-            Q1 = X[fix_cols].quantile(0.25)
-            Q3 = X[fix_cols].quantile(0.75)
+            Q1 = X_copy[fix_cols].quantile(0.25)
+            Q3 = X_copy[fix_cols].quantile(0.75)
 
             IQ = Q3 - Q1
 
             lower_bound = Q1 - self.factor * IQ
             upper_bound = Q3 + self.factor * IQ
 
-            X[fix_cols] = X[fix_cols].clip(lower=lower_bound, upper=upper_bound, axis=1)
+            X_copy[fix_cols] = X_copy[fix_cols].clip(lower=lower_bound, upper=upper_bound, axis=1)
 
-            return X  
+            return X_copy  
         
         elif self.method == 'z_score':
 
-            z_scores = np.abs(zscore(X[numeric_col]))
+            z_scores = np.abs(zscore(X_copy[numeric_col]))
 
-            return X[(z_scores < self.threshold).all(axis = 1)]
+            return X_copy[(z_scores < self.threshold).all(axis = 1)]
 
+
+
+class ProperDtypes(BaseEstimator, TransformerMixin):
+    '''
+    This class is helpful in trasnfoming certain columns to the appropriate column type
+
+    Parameters:
+    -----------
+        cols(list): List of columns that need to be transformed
+        Proper_type(str): The data type you want the cols to be transformed to
+
+    Returns:
+    --------
+        pd.DataFrame: Dataframe with the proper datatypes
+    '''
+
+    def __init__ (self, data: pd.DataFrame ,cols: list, proper_type: str):
+        self.data = data
+        self.cols = cols
+        self.proper_type = proper_type
+
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X, y=None):
+
+        X_transformed = X.copy()
+
+        X_transformed['Date'] = pd.to_datetime(X_transformed['Date'])
+        X_transformed[self.cols] =  X_transformed[self.cols].astype(self.proper_type)
+
+        return X_transformed
